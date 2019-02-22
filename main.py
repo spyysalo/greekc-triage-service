4785ebab7bc1e45c7366c64ec3f150b4dc354d8c
Felipe Soares <soarescmsa@gmail.com>
2019-02-22 11:18:57 +0100

    Added support to subprocess

--- main.py
+++ main.py
@@ -2,10 +2,84 @@ import requests
 
 from flask import Flask
 from flask import render_template
+import subprocess
+from threading import Thread
+from queue import Queue, Empty
+import json
 
 
 app = Flask(__name__)
 
+
+
+class NonBlockingStreamReader:
+    # from https://gist.github.com/EyalAr/7915597
+    def __init__(self, stream):
+        '''
+        stream: the stream to read from.
+                Usually a process' stdout or stderr.
+        '''
+
+        self._s = stream
+        self._q = Queue()
+
+        def _populateQueue(stream, queue):
+            '''
+            Collect lines from 'stream' and put them in 'quque'.
+            '''
+
+            while True:
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+                else:
+                    raise UnexpectedEndOfStream
+
+        self._t = Thread(target=_populateQueue,
+                         args=(self._s, self._q))
+        self._t.daemon = True
+        self._t.start()  # start collecting lines from the stream
+
+
+    def readline(self, timeout=None):
+        try:
+            return self._q.get(block=timeout is not None,
+                               timeout=timeout)
+        except Empty:
+            return None
+
+
+class UnexpectedEndOfStream(Exception): pass
+
+
+
+class Delft_Class(object):
+    def __init__(self):
+        """
+        Initialize the subprocess that will receive the inputs from the user
+        """
+        self._tagger = subprocess.Popen('python3 greekClassifier.py classify', shell=True,
+                                        stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        sleep(10)
+
+        self._nbsr = NonBlockingStreamReader(self._tagger.stdout)
+        sleep(2)
+
+    def get_result(self, text):
+
+
+        results = list()
+
+        self._tagger.stdin.write((text + '\n').encode('utf-8'))
+        self._tagger.stdin.flush()
+        output = self._nbsr.readline(0.1)
+        result_tmp = json.load(output.strip())
+        result = result_tmp['classifications'][0]['1']
+
+        return result
+
+
+Classifier = Delft_Class()
 
 def pubtator_url(pmid, concept='BioConcept', format_='JSON'):
     u = 'https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/'\
@@ -31,7 +105,8 @@ def get_pubtator_data(pmid):
 @app.route('/triage/<pmid>')
 def triage(pmid):
     data = get_pubtator_data(pmid)
-    return render_template('base.html', text=data['text'])
+    probability = Classifier.get_result(data['text'])
+    return render_template('base.html', text=data['text'], probability=probability)
 
 
 @app.route('/')
