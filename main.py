1cf75bf9d59f33c4a2aa4f77f52ba63e2716aa5f
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-02-22 10:24:40 +0100

    request pubtator data

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
+import requests
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
+@app.route('/triage/<pmid>')
+def triage(pmid):
+    r = requests.get('https://www.ncbi.nlm.nih.gov/CBBresearch/Lu/Demo/RESTful/tmTool.cgi/Chemical/{}/JSON/'.format(pmid))
+    return str(r.json())
+
+
 @app.route('/')
 def root():
-    return 'hi'
+    return '<html><body>Try here: <a href="triage/123">triage/123</a></body></html>'
